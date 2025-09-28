35089b7ebcfc897ab73ab9d2890b2e41c3bfacc1
Israel <israeloloruntoba3@email.com>
2025-09-28 01:27:21 +0100

    Add react-icons dependency and update Footer component with social media icons and contact details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import Logo from '../assets/shape-30.logo.png'
+import { TiSocialTwitter } from "react-icons/ti";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
@@ -17,16 +24,24 @@ const Footer = () => {
                     <li className='text-white hover:text-[#CBFE33]'><a href="#">Security</a></li>
                 </ul>
             </div>
+
             <hr className='border-gray-100 mb-6' />
+            
             <div className='flex space-x-7 space-y-5 sm:space-y-0 flex-wrap items-center justify-center text-white text-md'>
-                <p>hello@skillbirdge.com</p>
-                <p>+91813232309</p>
-                <p>Somewhere in the World</p>
+                <p className='flex items-center gap-2'><CiMail size={20} /> hello@skillbirdge.com</p>
+                <p className='flex items-center gap-2'><FaPhoneAlt />  +91813232309</p>
+                <p className='flex items-center gap-2'><FaMapMarkerAlt /> Somewhere in the World</p>
             </div>
             <hr className='border-gray-100 my-6' />
-            <div className='border-2 border-gray-100 rounded-2xl'>
-                <div>
-                    <div className='rounded-full p-2 bg-[#CBFE33]'></div>
+            <div className='border-[1px] border-gray-100 mt-7 p-5 place-items-center rounded-2xl'>
+                <div className='flex relative gap-2 -top-10 w-fit'>
+                    <div className='rounded-full w-fit p-2 bg-[#CBFE33]'><TiSocialFacebook size={25} /></div>
+                    <div className='rounded-full w-fit p-2 bg-[#CBFE33]'><TiSocialTwitter size={25} /></div>
+                    <div className='rounded-full w-fit p-2 bg-[#CBFE33]'><TiSocialLinkedin size={25} /></div>
+                </div>
+                <div className='-mt-5'>
+                    <p className='text-md text-gray-100 mb-3'>YourBank All Rights Reserved</p>
+                    <p className='text-md text-gray-100'>Privacy Policy | Tems of Service</p>
                 </div>
             </div>
         </div>
